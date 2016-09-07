7e17f4adfd39be1a20bf2daffb86651daebd37c6
cai_z <cai_z@worksap.co.jp>
2016-09-07 11:45:22 +0800

    init and create Cache and Log

---
+++ TPM.Framework/TPM.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TPM.Web.MvcApplication" Language="C#" %>
---
+++ TPM.Framework/TPM.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TPM.Web.Startup))]
+namespace TPM.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
